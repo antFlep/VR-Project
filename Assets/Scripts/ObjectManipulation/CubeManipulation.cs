e05fc9774727b4def691b7225370e8e9c53b4833
Filipe <fa5299@hotmail.com>
2019-02-11 20:59:46 +0100

    Started implementing expand and shrink capabilities

---
+++ Assets/Scripts/ObjectManipulation/CubeManipulation.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManipulation : MonoBehaviour
+{
+    float scale = 1;
+    float scaleFactor = 0.01f;
+    public string mode = "none";
+    public bool isActive = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            if (mode.Equals("none"))
+            {
+                Debug.Log("noting to Do");
+            }
+            if (mode.Equals("shrink"))
+            {
+                scale -= scaleFactor;
+
+                if (scale < 0)
+                {
+                    scale += scaleFactor;
+                    mode = "none";
+                    isActive = false;
+                    Debug.Log("Cannot be Shrunk further");
+                }
+                ExpandAndShrink();
+                Debug.Log("Shrinking");
+            }
+            if (mode.Equals("expand"))
+            {
+                scale += scaleFactor;
+                ExpandAndShrink();
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Object was triggerd");
+    }
+
+    void ExpandAndShrink()
+    {
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+}
---
+++ Assets/Scripts/ObjectSelection/RayCastSelect.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastSelect : MonoBehaviour
+{
+    Camera mainCamera;
+    int range = 100;
+    string mode = "none";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set Selection Mode
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            mode = "expand";
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            mode = "shrink";
+
+        // Select something
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Raycast Direction
+            Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+
+            // Raycast for hitdetection
+            RaycastHit hit;
+            Ray ray = new Ray(transform.position, forward);
+            Debug.DrawRay(ray.origin, ray.direction * range, Color.red, 1f);
+            bool result = Physics.Raycast(ray, out hit, range);
+
+            // If something was hit
+            if (result)
+            {
+                Vector3 laserEnd = hit.point;
+                Debug.Log(hit.transform.name);
+                CubeManipulation cube = hit.transform.GetComponent<CubeManipulation>();
+                if (cube)
+                {
+                    cube.isActive = true;
+                    cube.mode = mode;
+                }
+            }
+        }
+
+        // Disable object and selection mode
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Disable object selection");
+        }
+    }
+}
