7c7e0052cdd028981793aacd38cbd7573672be92
Filipe <fa5299@hotmail.com>
2019-03-06 22:59:20 +0100

    Weld objects highlighting and basic object spawner implemented
    Not tested

--- Assets/Scripts/ObjectManipulation/WeldWrapper.cs
+++ Assets/Scripts/ObjectManipulation/WeldWrapper.cs
@@ -5,17 +5,43 @@ using UnityEngine;
 public class WeldWrapper : MonoBehaviour
 {
     public ModeSwitcher modeSwitcher;
+    public Material weldMaterial;
+
+    Material origMaterial;
+    MeshRenderer renderer;
+    public bool highlight = false;
 
     // Start is called before the first frame update
     void Start()
     {
         modeSwitcher = GameObject.Find("MasterObject").GetComponent<ModeSwitcher>();
+        if (GetComponent<MeshRenderer>())
+        {
+            renderer = GetComponent<MeshRenderer>();
+            origMaterial = renderer.material;
+        }
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (modeSwitcher.weldObject1.Equals(this.gameObject) || modeSwitcher.weldObject2.Equals(this.gameObject) || highlight)
+        {
+            if (renderer)
+            {
+                //origMaterial = renderer.material;
+                renderer.material = weldMaterial;
+                HighlightChildren();
+            }
+        } else
+        {
+            if(renderer)
+            {
+                renderer.material = origMaterial;
+                UnHighlightChildren();
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider collider)
@@ -30,6 +56,38 @@ public class WeldWrapper : MonoBehaviour
         {
             Debug.Log("Obj2");
             modeSwitcher.weldObject2 = transform.gameObject;
+        }
+    }
+
+    private void HighlightChildren()
+    {
+        if (transform.childCount > 0)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Transform child = transform.GetChild(i);
+                WeldWrapper childWeld = child.GetComponent<WeldWrapper>();
+                if (childWeld)
+                {
+                    childWeld.highlight = true;
+                }
+            }
+        }
+    }
+
+    private void UnHighlightChildren()
+    {
+        if (transform.childCount > 0)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Transform child = transform.GetChild(i);
+                WeldWrapper childWeld = child.GetComponent<WeldWrapper>();
+                if (childWeld)
+                {
+                    childWeld.highlight = false;
+                }
+            }
         }
     }
 }
