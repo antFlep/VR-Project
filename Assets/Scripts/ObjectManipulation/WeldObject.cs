2a27d3826bd146eb03e8a47e5b09f73c3a345ec9
Highrules <fahaba@web.de>
2019-03-06 04:04:03 +0100

    implemented weld script and made new prefabs of exact objects (unwrapped UW for applying textures/materials

--- Assets/Scripts/ObjectManipulation/WeldObject.cs
+++ Assets/Scripts/ObjectManipulation/WeldObject.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class WeldObject : MonoBehaviour
 {
@@ -16,12 +17,50 @@ public class WeldObject : MonoBehaviour
         
     }
 
-    public static void Weld(GameObject object1, GameObject object2)
+    public void Weld(GameObject object1)
     {
         GameObject newObject = new GameObject();
+        object1.transform.SetParent(newObject.transform);
+        Destroy(object1.GetComponent<Throwable>());
+        Destroy(object1.GetComponent<VelocityEstimator>());
+        Destroy(object1.GetComponent<Interactable>());
+        Destroy(object1.GetComponent<Rigidbody>());
+        object1.tag = "Untagged";
+        //object2.tag = "default";
+
+        //object2.transform.SetParent(newObject.transform);
+        //Destroy(object2.GetComponent<Throwable>());
+        //Destroy(object2.GetComponent<VelocityEstimator>());
+        //Destroy(object2.GetComponent<Interactable>());
+        //Destroy(object2.GetComponent<Rigidbody>());
+
+        newObject.AddComponent<Rigidbody>();
+        newObject.tag = "weldable";
+        newObject.AddComponent<Throwable>();
+    }
+
+    public void destroyObject(GameObject originObject)
+    {
 
         
-        if (object1.GetComponentInChildren<BaseObject>() != null)
-            Destroy(object1.get)
+        if (originObject.transform.Find("BaseCube") != null)
+        {
+            GameObject obj = originObject.transform.Find("BaseCube").gameObject;
+            Destroy(obj);
+        }
     }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        
+        GameObject obj = collision.gameObject;
+        obj = obj.transform.root.gameObject;
+        Debug.Log(obj.name);
+        if (obj.CompareTag("weldable"))
+        {
+            destroyObject(obj);
+            Weld(obj);
+        }        
+    }
+
 }
